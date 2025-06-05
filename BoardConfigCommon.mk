105d6d80a4a69bbfb188dbab240663b0b85b08ba
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:05:41 +0800

    Disable SELinux

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -69,3 +69,4 @@ include device/amlogic/common/BoardConfigAmlogic.mk
 
 ## Include the common proprietary BoardConfig makefile
 include vendor/amlogic/g12-common/BoardConfigVendor.mk
+BOARD_BOOTCONFIG += androidboot.selinux=permissive
